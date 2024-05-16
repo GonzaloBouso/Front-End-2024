1ffaf430132ae3934e92cf338699d10907f68b2f
GonzaloBouso <gonzalobouso002@gmail.com>
2024-05-16 02:53:30 -0300

    agregue la seccion del contador

--- TP3/src/components/Contador.jsx
+++ TP3/src/components/Contador.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { useState } from "react"
+import './contador.css'
+
 
 export const Contador = () => {
+  const [contador, setContador] = useState(0);
+  const incrementarContador = () => {
+    setContador(contador + 1);
+  }
   return (
-    <div>Contador</div>
+    <div className="contador-container">
+      <h2 className="contador-title">Contador</h2>
+      <div className="contador-value">{contador}</div>
+      <button className="contador-button" onClick={incrementarContador}>
+        Incrementar</button>
+    </div>
   )
 }
